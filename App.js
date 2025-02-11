49011956b17bac51de686970372ec3a28f6a540a
Logapriyan <140060244+logapriyanr@users.noreply.github.com>
2025-02-12 01:18:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Home from './pages/home';
+import './App.css';
+
+function App() {
+  return(
+    <div className='App'>
+      <div  className="bg-img">
+      <Home/>
+      </div>
+      
+    </div>
+  )
+}
+export default App;
